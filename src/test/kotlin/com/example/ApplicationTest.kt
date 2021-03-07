ebfb6e9833d1d166bf4a19cd9cb43d032212652e
Jake Sikora <jakesikora.01@gmail.com>
2021-03-06 22:34:46 -0500

    Added tests for LIST and POST customer

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,11 +1,19 @@
 package com.example
 
+import com.example.models.Customer
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.testing.handleRequest
+import io.ktor.server.testing.setBody
 import io.ktor.server.testing.withTestApplication
 import kotlin.test.Test
 import kotlin.test.assertEquals
+
+val objectMapper = jacksonObjectMapper()
 
 class ApplicationTest {
     @Test
@@ -17,4 +25,31 @@ class ApplicationTest {
             }
         }
     }
+
+    @Test
+    fun `test customer`() {
+        withTestApplication({ serve() }) {
+            handleRequest(HttpMethod.Get, "/customer").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                val expected = listOf(baseCustomer, baseCustomer.copy(id=2))
+                val actual: List<Customer> = objectMapper.readValue(response.content!!)
+                assertEquals(expected, actual)
+            }
+        }
+    }
+
+    @Test
+    fun `Create Customer`() {
+        withTestApplication({ serve() }) {
+            handleRequest(HttpMethod.Post, "/customer") {
+                addHeader(HttpHeaders.ContentType, ContentType.Application.Json.toString())
+                setBody(objectMapper.writeValueAsString(baseCustomer))
+            }.apply {
+                assertEquals(HttpStatusCode.Created, response.status())
+                val actual: Customer = objectMapper.readValue(response.content!!)
+                assertEquals(baseCustomer, actual)
+            }
+        }
+    }
+
 }
